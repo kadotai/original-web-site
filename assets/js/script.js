c4c1eb201a16927df143116be10154f386f8036d
kadotai <kado1311@gmail.com>
2024-10-23 10:50:05 +0800

    スライド

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,13 +36,20 @@ fourth.addEventListener('mouseout', function() {
     this.style.background= '';
 });
 
+// Display Function
 $(function(){
     $('#think').on('click',function(){
+        const boxContainer = document.getElementById("pc1");
+        console.log(boxContainer);
         $(this).nextAll().slideToggle();
+        boxContainer.style.display = "flex";
     });
 
     $('#watch').on('click',function(){
+        const boxContainer_2 =document.getElementById("pc2");
+        console.log(boxContainer_2);
         $(this).nextAll().slideToggle();
+        boxContainer_2.style.display = "flex";
     });
 
     $('#summary').on('click',function(){
